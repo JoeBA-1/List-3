0283e763176c84a8aa3e2ea199970f90ce3d75f0
JoeBA <135997793+JoeBA-1@users.noreply.github.com>
2023-07-26 21:22:47 +0300

    for loop bug fixed

---
+++ README.md
@@ -0,0 +1,2 @@
+# List-3
+# https://bejewelled-khapse-77d37f.netlify.app
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,27 @@
-let loginBtn = document.getElementById("login")
+const accounts = [['admin01', '123'] , ['admin02',"234"], ['admin03',"345"]]
+const loginBtn = document.getElementById("login")
 let usernameEl = document.getElementById("username")
 let passwordEl = document.getElementById("password")
+
 
 document.title = "Login Page"
 
 loginBtn.addEventListener("click",function(){
     let accessUser = usernameEl.value
-    let accessPass = passwordEl.value
-    console.log(accessUser , accessPass)    
+    let accessPass = passwordEl.value 
+    let found = false
 
+    // for (let i=0; i<accounts.length; i++){
+    for (let i in accounts){
+        if(accessUser == accounts[i][0] && accessPass == accounts[i][1]){
+            alert(`welcome ${accessUser}`)
+            found = true
 
-
-
-
+        }
+    }
+    if(found == false){
+            alert('invalid Username or Password')
+        }
 
 
 })
