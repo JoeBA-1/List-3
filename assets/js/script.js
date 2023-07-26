f9e8ce8625335b7940e4c701117a6997edb52ec2
JoeBA <135997793+JoeBA-1@users.noreply.github.com>
2023-07-26 19:25:57 +0300

    add assets and index.html

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+let loginBtn = document.getElementById("login")
+let usernameEl = document.getElementById("username")
+let passwordEl = document.getElementById("password")
+
+document.title = "Login Page"
+
+loginBtn.addEventListener("click",function(){
+    let accessUser = usernameEl.value
+    let accessPass = passwordEl.value
+    console.log(accessUser , accessPass)    
+
+
+
+
+
+
+
+})
